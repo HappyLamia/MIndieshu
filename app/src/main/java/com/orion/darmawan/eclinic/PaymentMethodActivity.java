581fa741f732fe7ff645e90e80116af7402dfa77
bayuaji911 <bayuaji.personal@gmail.com>
2018-10-19 19:28:16 +0700

    Adding New Activity
    - Daftar Produk
    - Daftar Produk Khusus
    - Cart
    - Payment Method
    - Checkout

---
+++ app/src/main/java/com/orion/darmawan/eclinic/PaymentMethodActivity.java
@@ -0,0 +1,13 @@
+package com.orion.darmawan.eclinic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PaymentMethodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_method);
+    }
+}
