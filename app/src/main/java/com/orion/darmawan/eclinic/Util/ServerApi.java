b85b44f5e576a77339ec45f4f70b50ed8e15c31f
HappyLamia <smenjensen@gmail.com>
2018-11-02 09:11:11 +0700

    Update API

--- app/src/main/java/com/orion/darmawan/eclinic/Util/ServerApi.java
+++ app/src/main/java/com/orion/darmawan/eclinic/Util/ServerApi.java
@@ -4,4 +4,5 @@ public class ServerApi {
     public static final String URL_PARENT = "https://demo.indieshu.net/";
     public static final String URL_SYNCH = URL_PARENT+"api/synch/user";
     public static final String URL_PRODUCT = URL_PARENT+"api/data/barang";
+    public static final String URL_DETAIL_PRODUCT = URL_PARENT+"api/data/get-detail-barang";
 }
